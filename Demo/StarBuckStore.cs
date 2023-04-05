b4efcb85bbcfff83babd27c95b92e2d54e201f06
MukulDevPatel <125425636+MukulDevPatel@users.noreply.github.com>
2023-04-05 18:39:34 +0530

    [Mukul Dev] Add: This is Star bucks detail

---
+++ Demo/StarBuckStore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMaker
+{
+    public class StarBuckStore
+    {
+        public readonly IMakeACoffee coffee;
+        public StarBuckStore(IMakeACoffee coffee)
+        {
+            this.coffee = coffee;
+        }
+        public string OrderACoffee(int sugarPerSpoon, int coffeePack)
+        {
+            if (coffee.CheckIngredientAvailability())
+            {
+                return coffee.CoffeeMaking(sugarPerSpoon, coffeePack);
+            }
+            else
+            {
+                return "Sorry! coffee is not available";
+            }
+        }
+    }
+}
